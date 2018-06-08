419405678e1cc8e5cc2c004ce0853e1098461ec9
critsmet <36425627+critsmet@users.noreply.github.com>
2018-06-07 20:59:46 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+require "pry"
+
+  class Artist
+    attr_accessor :name, :genre
+
+    @@all = []
+
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def new_song(name, genre)
+      Song.new(name, self, genre)
+    end
+
+    def songs
+      Song.all.select do |songs|
+        songs.artist == self
+      end
+    end
+
+    def genres
+      Song.all.map do |songs|
+        songs.genre
+      end.uniq
+    end
+end
+
+# Pry.start
